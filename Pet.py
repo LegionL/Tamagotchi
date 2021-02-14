0350c58168099a3685ef56bd9c95b79a61a40c5a
Weiran Lin <kqe2705@gmail.com>
2021-02-15 03:13:39 +1100

    stage finished

--- Pet.py
+++ Pet.py
@@ -1,29 +1,51 @@
 from abc import ABC, abstractmethod
 import time
+from enum import Enum
+
+
+class State(Enum):
+    SLEEP = 1
+    AWAKE = 2
+    DEAD = 3
 
 
 class Pet(ABC):
-
     @abstractmethod
     def eat(self):
         pass
 
+    @abstractmethod
+    def drink(self):
+        pass
+
+    @abstractmethod
+    def gain_energy(self):
+        pass
+
+    @abstractmethod
+    def gain_health(self):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
+
 
 class Cat(Pet):
-
     def __init__(self, ):
         t0 = time.time()
         self.age = 0
+        self.age_updated_time = t0
 
-        self.health = 80
+        self.health = 1
         self.max_health = 100
         self.health_updated_time = t0
 
-        self.food = 8
+        self.food = 5
         self.max_food = 10
         self.food_updated_time = t0
 
-        self.water = 8
+        self.water = 5
         self.max_water = 10
         self.water_updated_time = t0
 
@@ -32,9 +54,16 @@ class Cat(Pet):
         self.energy_updated_time = t0
 
         self.location = None
-        self.state = None
+        self.state = State.AWAKE
 
         self.stage = None
+        self.poop_sensor = False
+        self.poop_updated_time = t0
+
+    def gain_age(self, diff=1):
+        self.age = self.age + int(diff)
+        self.age = max(0, self.age)
+        self.age_updated_time = time.time()
 
     def eat(self, diff=1):
         self.food = self.food + int(diff)
@@ -57,49 +86,66 @@ class Cat(Pet):
         self.health_updated_time = time.time()
 
     def update(self, ):
+        if self.state == State.DEAD:
+            self.stage.add_notifications(f'[Dead]   the {self.__class__.__name__} dies.')
+            return -1 
+
         t0 = time.time()
-        sleep_modifier = 1 if self.state != 'sleep' else 3
+        sleep_modifier = 1 if self.state != State.SLEEP else 3
+        self.poop_sensor = 'poop' in self.stage.items
+        poop_modifier = 1 if self.poop_sensor == False else 0.1
+
+        diff, _ = divmod(t0 - self.age_updated_time, 60)
+        if diff != 0:
+            self.gain_age(diff)
 
         diff, _ = divmod(t0 - self.food_updated_time, 10 * sleep_modifier)
-        self.eat(-diff)
+        if diff != 0:
+            self.eat(-diff)
 
         diff, _ = divmod(t0 - self.water_updated_time, 20 * sleep_modifier)
-        self.drink(-diff)
+        if diff != 0:
+            self.drink(-diff)
 
-        diff, _ = divmod(t0 - self.energy_updated_time, 30)
-        self.gain_energy(-diff)
+        diff, _ = divmod(t0 - self.energy_updated_time, 20)
+        if diff != 0:
+            self.gain_energy(-diff)
+
+        diff, _ = divmod(t0 - self.poop_updated_time, 20)
+        if diff != 0:
+            self.poop()
 
         diff, _ = divmod(t0 - self.health_updated_time, 20 * sleep_modifier)
-        if self.food >= 5 or self.water >= 5:
-            self.gain_health(diff)
-        else:
-            self.gain_health(-diff)
+        if diff != 0:
+            if self.food >= 5 or self.water >= 5:
+                self.gain_health(diff)
+            else:
+                self.gain_health(-diff)
 
-    def poop(self, ):
-        self.stage.append('poop')
+        if self.health <= 0 or self.age >= 20:
+            self.die()
+
+    def die(self, ):
+        self.state = State.DEAD
+
+    def poop(self, forced=False):
+        self.stage.items.append('poop')
+        if forced:
+            self.stage.add_notifications(f'[Poop]   the {self.__class__.__name__} poops RELUCTANTLY.')
+        else:
+            self.stage.add_notifications(f'[Poop]   the {self.__class__.__name__} poops.')
+        self.poop_updated_time = time.time()
 
     def sleep(self, ):
         t0 = time.time()
-        self.state = 'sleep'
+        self.state = State.SLEEP
         diff, _ = divmod(t0 - self.energy_updated_time, 10)
         self.gain_energy(diff)
 
     def wake_up(self, ):
-        self.state = 'idle'
+        self.state = State.AWAKE
 
     def __str__(self, ):
-        return f'Pet: Cat  Age: {self.age}  Food: {self.food}  '\
-               f'Water: {self.water}  Energy: {self.energy}'
+        return f'Cat  State: {self.state.name}  Age: {self.age}  Health: {self.health}  '\
+               f'Food: {self.food}  Water: {self.water}  Energy: {self.energy}'
 
-
-if __name__ == '__main__':
-    c = Cat()
-    c.eat()
-    print(c)
-    c.eat()
-    print(c)
-    c.eat()
-    print(c)
-    time.sleep(20)
-    c.update()
-    print(c)
--- Tamagotchi.py
+++ Tamagotchi.py
@@ -1,28 +1,89 @@
 from Pet import *
 import threading
+import sys
 import time
 import click
+import os
 
 
-def user_input(input_buffer_size=10):
+class Stage:
+    def __init__(self, row_num=30, col_num=30):
+        # self.pixels = [['  ' for c in range(col_num)] for r in range(row_num)]
+        # for i in range(row_num):
+        #     for j in range(col_num):
+        #         if i == 0 or j == 0 or i == row_num - 1 or j == col_num - 1:
+        #             self.pixels[i][j] = '██'
+
+        self.items = []
+        self.number_of_notifications_shown = 20
+        self.notifications = [''] * self.number_of_notifications_shown
+
+    def add_notifications(self, line):
+        self.notifications.append(line)
+        while len(self.notifications) > self.number_of_notifications_shown:
+            self.notifications = self.notifications[1:]
+        stage.draw()
+
+    def draw(self, ):
+        clear_command_line()
+        print('\n'.join(self.notifications + ['\n' + '-' * 20 + '\n']))
+        self.prompt()
+        
+    def prompt(self, ):
+        print(f'1 - Show Status  2 - Eat  3 - Drink  4 - Play  '\
+              f'5 - Clean  6 - Poop  q - Quit\n')
+
+
+def user_input(input_buffer_size=1):
     global cmd
     while True:
-        cmd.append(click.getchar())
-        while len(cmd) > input_buffer_size:
-            cmd = cmd[1:]
+        cmd = click.getchar()
+        # cmd.append(click.getchar())
+        # while len(cmd) > input_buffer_size:
+        #     cmd = cmd[1:]
 
+def clear_command_line():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-cmd = []
+cmd = None
 thread_user_input = threading.Thread(target=user_input)
 thread_user_input.daemon = True
 thread_user_input.start()
 
-stage = []
-c = Cat()
-stage.append(c)
+stage = Stage()
+pet = Cat()
+stage.items.append(pet)
+pet.stage = stage
+stage.add_notifications('Generating a cat')
+
+stage.draw()
 while True:
-    print(cmd)
-    if cmd and cmd[-1] == 'q':
+    if cmd == 'q':
         break
-    cmd = []
+    elif cmd == '1':
+        stage.add_notifications(f'[Status] {pet}') 
+    elif cmd == '2':
+        pet.eat()
+        stage.add_notifications(f'[Eat]    {pet.__class__.__name__} eats a can of catfood')
+    elif cmd == '3':
+        pet.drink()
+        stage.add_notifications(f'[Drink]  {pet.__class__.__name__} drinks some water')
+    elif cmd == '4':
+        pass
+    elif cmd == '5':
+        prev = len(stage.items)
+        stage.items = [i for i in stage.items if i != 'poop']
+        stage.add_notifications(f'[Clean]  {prev - len(stage.items)} poop cleaned')
+    elif cmd == '6':
+        pet.poop(forced=True)
+
+    cmd = None
+
+    for i in stage.items:
+        if isinstance(i, Pet):
+            if i.update() == -1:
+                stage.add_notifications(f'[System] will exit in 3 seconds...')
+                time.sleep(3)
+                sys.exit()
+    
     time.sleep(0.1)
